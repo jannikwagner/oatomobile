834c8530db49c2ece24786e9a3e94430f2410d02
Jannik Wagner <jannik.wagner@students.uni-mainz.de>
2021-03-11 13:45:58 +0100

    device configured

--- evaluate.py
+++ evaluate.py
@@ -25,13 +25,13 @@ from typing import List, Mapping
 import pandas as pd
 
 
-def getDIM(path=None, mobilenet_num_classes=128) -> ImitativeModel:
+def getDIM(path=None,device="cpu", mobilenet_num_classes=128) -> ImitativeModel:
     if path is None:
         path = os.path.join(MODELS_PATH, "dim", "9", "ckpts", "model-96.pt")
     model = ImitativeModel(mobilenet_num_classes=mobilenet_num_classes)
     x = torch.load(path)
     model.load_state_dict(x)
-    model.eval()
+    model.eval().to(device)
     return model
 
 
@@ -71,7 +71,7 @@ def minFDE(samples: List[torch.Tensor], ground_truth: torch.Tensor):
     return torch.stack(samples_FDE).min(axis=0)
 
 def evaluate(ckpt_path, data_path, output_path, mobilenet_num_classes=128):
-    model = getDIM(ckpt_path, mobilenet_num_classes)
+    model = getDIM(ckpt_path, device, mobilenet_num_classes)
     modalities = (
         "lidar",
         "is_at_traffic_light",
@@ -119,7 +119,7 @@ def evaluate(ckpt_path, data_path, output_path, mobilenet_num_classes=128):
             FDE_100s.append(FDE(y_100, ground_truth))
             minFDE_ks.append(minFDE(samples, ground_truth))
     measure_lists = ADE_ms,ADE_10s,ADE_100s,minADE_ks,FDE_ms,FDE_10s,FDE_100s,minFDE_ks
-    measures = [torch.stack(measure).mean() for measure in measure_lists]
+    measures = [torch.stack(measure).mean().item() for measure in measure_lists]
     ADE_m,ADE_10,ADE_100,minADE_k,FDE_m,FDE_10,FDE_100,minFDE_k = measures
     names = "ADE_m","ADE_10","ADE_100","minADE_k","FDE_m","FDE_10","FDE_100","minFDE_k"
     vals = [[val] for val in measures]
